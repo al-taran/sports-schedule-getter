f901b69955a6165f39b5379fb2467f75da2a5c2f
shadypollen <shadypollen@protonmail.com>
2024-01-21 02:51:32 +0000

    Implement scrolling through all of the search results

--- main.py
+++ main.py
@@ -1,26 +1,70 @@
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 ELG_URL = "https://www.google.com/search?hl=en&q=euroleague%20schedule#sie=lg;/g/11kk5tfhf5;3;/m/0b740cl;mt;fp;1;;;"
 NBA_URL = "https://www.google.com/search?hl=en&q=nba%20schedule#sie=lg;/g/11snv1vp6v;3;/m/05jvx;mt;fp;1;;;"
 
-driver = webdriver.Firefox()
 
+def scroll_down(driver):
+    """A method for scrolling the page."""
+    els = driver.find_elements(By.CLASS_NAME, 'GVj7ae')
+    # Get num of elements
+    ll = len(els)
+    print("ll", ll)
+    while True:
+        print("scrolling")
+        # Scroll down to the bottom.
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script("arguments[0].scrollIntoView();", els[-1])
+        # Wait to load the page.
+        time.sleep(5)
+        # See if new els are present
+        els = driver.find_elements(By.CLASS_NAME, 'GVj7ae')
+        nl = len(els)
+        print("nl", nl)
+
+        if ll == nl:
+            break
+        ll = nl
+
+driver = webdriver.Firefox()
 driver.get("https://www.google.com/404error")
 driver.add_cookie({"name": "CONSENT", "value": "YES+cb.20240114-08-p0.cs+FX+111"})
 driver.get(ELG_URL)
+time.sleep(5)
+scroll_down(driver)
 page_html = driver.page_source
+driver.close()
 
 
 soup = BeautifulSoup(page_html, "html.parser")
 trs = soup.find_all(attrs={'data-start-time': True})
 
+games = {}
+trs_counter = 0
 for tr in trs:
-    print(tr['data-start-time'])
+    trs_counter += 1
+    game_time = tr['data-start-time']
     tds = tr.find_all("td")
-    print(tds[13].find("span").decode_contents())
-    print(tds[16].find("span").decode_contents())
+    team_one = tds[13].find("span").decode_contents()
+    team_two = tds[16].find("span").decode_contents()
+    teams = [team_one, team_two]
+    teams.sort()
+
+    team_hash = game_time + teams[0] + teams[1]
+
+    if team_hash not in games:
+        games[team_hash] = {'game_time': game_time, 'team_home': team_one, 'team_away': team_two}
+    else:
+        # The games that are shown when you first load the search
+        print("dupe found:", team_hash)
+
+for key in list(games.keys()):
+    print(key)
+print("trs:", trs_counter, "games:", len(games))
+
 
 #for td in tds[13:]:
 #    print(td.prettify())
